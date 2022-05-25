68504751fe4a5aa56ed23627c7efe4bdd7c31c5b
pessoadavi <pessoadavi@gmail.com>
2022-05-25 11:43:08 -0300

    Verificando a ação do ribbon no load balancing.

--- loja/src/main/java/br/com/microservice/loja/service/CompraServiceImpl.java
+++ loja/src/main/java/br/com/microservice/loja/service/CompraServiceImpl.java
@@ -1,6 +1,7 @@
 package br.com.microservice.loja.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -13,11 +14,15 @@ import br.com.microservice.loja.model.dto.InfoFornecedorDto;
 public class CompraServiceImpl implements CompraService {
 
 	private @Autowired RestTemplate client;
+	private @Autowired DiscoveryClient eurekaClient;
 	
 	@Override
 	public void realizaCompra(CompraDto compra) {
 				
 		ResponseEntity<InfoFornecedorDto> exchange = client.exchange("http://fornecedor:8081/info/"+compra.getEndereco().getEstado(), HttpMethod.GET, null, InfoFornecedorDto.class);
+		
+		eurekaClient.getInstances("fornecedor").stream().forEach(c -> { System.out.println("localhost: "+c.getPort());});
+		
 		System.out.println(exchange.getBody().getEndereco());
 	}
 
