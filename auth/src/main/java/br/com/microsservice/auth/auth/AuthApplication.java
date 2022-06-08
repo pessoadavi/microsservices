3cee2163587259015ce4b8f1db1af3aa688a57f3
pessoadavi <pessoadavi@gmail.com>
2022-06-08 15:22:43 -0300

    Alteração.

--- auth/src/main/java/br/com/microsservice/auth/auth/AuthApplication.java
+++ auth/src/main/java/br/com/microsservice/auth/auth/AuthApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -19,7 +19,7 @@ public class AuthApplication {
 		SpringApplication.run(AuthApplication.class, args);
 	}
 	
-	@PostMapping("/user")
+	@GetMapping("/user")
 	public Principal user(Principal user) {
 		return user;
 	}
